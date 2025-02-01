84d59a86db192ab55adcfc6eed9033dc24ff4301
Tobias Kunze <r@rixx.de>
2025-02-01 01:59:27 +0100

    Initial commit

---
+++ pretalx_friendlycaptcha/signals.py
@@ -0,0 +1,23 @@
+
+from django.dispatch import receiver
+from django.urls import reverse
+
+from pretalx.orga.signals import nav_event_settings
+
+
+@receiver(nav_event_settings)
+def pretalx_friendlycaptcha_settings(sender, request, **kwargs):
+    if not request.user.has_perm("orga.change_settings", request.event):
+        return []
+    return [
+        {
+            "label": "FriendlyCaptcha CfP step",
+            "url": reverse(
+                "plugins:pretalx_friendlycaptcha:settings",
+                kwargs={"event": request.event.slug},
+            ),
+            "active": request.resolver_match.url_name
+            == "plugins:pretalx_friendlycaptcha:settings",
+        }
+    ]
+
