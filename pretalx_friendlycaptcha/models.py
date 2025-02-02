418e0966bcda2cee61f86b176ef3fe34fcbfa8ba
Tobias Kunze <r@rixx.de>
2025-02-02 12:23:19 +0100

    Add missing settings

--- pretalx_friendlycaptcha/models.py
+++ pretalx_friendlycaptcha/models.py
@@ -7,4 +7,25 @@ class FriendlycaptchaSettings(models.Model):
         on_delete=models.CASCADE,
         related_name="pretalx_friendlycaptcha_settings",
     )
-    site_key = models.CharField(max_length=200, default="")
+    secret = models.CharField(max_length=200, default="")
+    site_key = models.CharField(max_length=200, default="", null=True, blank=True)
+    endpoint = models.CharField(
+        max_length=200,
+        choices=[("US", "US"), ("EU", "EU")],
+        default="US",
+        help_text="If you are on the Advanced or Enterprise plan, you can set the location of the endpoint to EU.",
+    )
+
+    @property
+    def base_url(self):
+        if self.endpoint == "US":
+            return "https://api.friendlycaptcha.com/api/v1/"
+        return "https://eu-api.friendlycaptcha.eu/api/v1/"
+
+    @property
+    def puzzle_url(self):
+        return self.base_url + "puzzle"
+
+    @property
+    def verify_url(self):
+        return self.base_url + "siteverify"
